deedc01092d22d0b8637be1e3083b18d5a60247d
VaibhavGholap <vaibhav.gholap5@gmail.com>
2024-05-08 11:39:14 +0530

    First Commit

---
+++ Demo1/src/test/java/com/test/LoginTest.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+@Test
+public class LoginTest {
+	
+	public void add() {
+		int a=10;
+		int b=20;
+		int c = a+b;
+		System.out.println("Addition is =" +c );
+	}
+
+}
